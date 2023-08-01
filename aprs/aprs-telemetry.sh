db79b05657a8f07ddfcf6db90ecaf02e042702ba
Steve <steve@jxqz.org>
2023-07-31 22:30:26 -0700

    telemetry random walk

--- aprs/aprs-telemetry.sh
+++ aprs/aprs-telemetry.sh
@@ -8,21 +8,45 @@
 #
 
 MYCALL=N0CALL
-MYPASS=12345
+PASSCODE=12345
 DEST=APRS
-
-SERVER=aserver.aprs2.net
+SERVER=server.aprs2.net
 PORT=14580
-
-SEQUENCE_FILE=~/.aprs_sequence
-
-# todo: getopt
 
 APRSIS=false
 METADATA=false
 
-# Option: APRS-IS
-# Option: RF
+SEQUENCE_FILE=~/.aprs_sequence
+CONFIG_FILE=~/aprs.config
+
+read_apc_ups()
+{
+        echo 'reading APC UPS'
+}
+
+while getopts 'him' OPT
+do
+        case $OPT in
+
+                i)
+                        APRSIS=true
+                        ;;
+
+                m)
+                        METADATA=true
+                        ;;
+
+                *)
+                        echo "usage: $0 [ -i ] [ -m ]"
+                        exit 1
+                        ;;
+        esac
+done
+
+if [ -f $CONFIG_FILE ]
+then
+	source $CONFIG_FILE
+fi
 
 if [ $APRSIS == true ]
 then
@@ -33,7 +57,7 @@ then
 
 	echo $LINE
 
-	echo "user $MYCALL pass $MYPASS vers aprs-telemetry 0.1\n" >&3
+	echo "user $MYCALL pass $PASSCODE vers aprs-telemetry 0.1\n" >&3
 
 	read -u 3 LINE # verify login
 
@@ -42,12 +66,12 @@ then
 	sleep 2
 fi
 
-if [ METADATA = true ]
+if [ $METADATA = true ]
 then
 
-	echo "$MYCALL>$DEST::$MYCALL    :PARM.VBAT" >&3
-	echo "$MYCALL>$DEST::$MYCALL    :UNIT.VDC" >&3
-	echo "$MYCALL>$DEST::$MYCALL    :EQNS.0,1,0" >&3
+	echo "$MYCALL>$DEST::$MYCALL    :PARM.BATV,LINEV,BATPCT" >&3
+	echo "$MYCALL>$DEST::$MYCALL    :UNIT.VDC,VAC,PCT" >&3
+#	echo "$MYCALL>$DEST::$MYCALL    :EQNS.0,1,0" >&3 ## defaults assumed
 	exit 0
 fi
 
@@ -63,11 +87,12 @@ fi
 
 SQFORMAT=$(printf "%03d" $SEQUENCE)
 
-echo "$MYCALL>$DEST:T#$SQFORMAT,13.6" >&3
-
-sleep 2
-
-exec 3>&-
+if [ $APRSIS == true ]
+then
+	echo "$MYCALL>$DEST:T#$SQFORMAT,13.$(($RANDOM % 100 / 10)),$((118 + $RANDOM % 10)),$(bc <<< "100.0 - 0.$(($RANDOM % 10))")" >&3
+	sleep 2
+	exec 3>&-
+fi
 
 let SEQUENCE=$SEQUENCE+1
 
